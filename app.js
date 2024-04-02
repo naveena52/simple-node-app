270c5307945629da2b2027f6db973632570b75a2
Naveena Ponna <Naveena@welkin-digital.com>
2024-04-02 16:26:34 +0530

    simple node app

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 let express = require('express');
 let app = express();
-let dotenv = require ('dotenv');
-dotenv.config()
-let port = process.env.PORT || 6700;
+let PORT = process.env.PORT || 3000;
 app.get('/',(req,res)=>{
 res.send("<h1>THIS IS SIMPLE NODE JS APP</h1>")
 })
